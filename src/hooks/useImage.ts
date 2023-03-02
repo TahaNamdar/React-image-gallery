325f7048eb7fd4ef26b1c8f03d9c4b85561d639a
TahaNamdar <tahanamdar30@gmail.com>
2023-03-02 16:15:59 +0330

    latest changes

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react'
 
-export const useImage = (fileNames: []) => {
-  const [image, setImage] = useState([] as any)
+export const useImage = (fileNames: {original:string,thumbnail:string}[]) => {
+  
+  const [image, setImage] = useState([] as ({original: string, thumbnail: string}[]))
 
   useEffect(() => {
     const fetchImage = async () => {
-      console.log(fileNames)
       for (let i = 0; i < fileNames.length; i++) {
         const file: { original: string; thumbnail: string } = fileNames[i]
         const res_original = await import(`../images/${file.original}`)
         const res_thumbnail = await import(`../images/${file.thumbnail}`)
 
-        setImage((prev: any) => {
+        setImage((prev) => {
           return [
             ...prev,
             {
